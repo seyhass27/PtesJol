74bd4701a3e607bd6850158d46c371d90848b95d
seyhass27 <sat.nuseyha27@gmail.com>
2018-07-01 23:54:03 +0700

    Create Adapter Class and Navigation layout

--- app/src/main/java/com/example/andriod/ptesjol/Activity/MainActivity.java
+++ app/src/main/java/com/example/andriod/ptesjol/Activity/MainActivity.java
@@ -1,13 +1,15 @@
-package com.example.andriod.ptesjol;
+package com.example.andriod.ptesjol.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.andriod.ptesjol.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.view_holder_houses);
     }
 }
