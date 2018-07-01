42f3215f6743ff9eca50b16eb66afc74b92f1efd
seyhass27 <sat.nuseyha27@gmail.com>
2018-07-02 00:24:24 +0700

    navigation update

--- app/src/main/java/com/example/andriod/ptesjol/Activity/MainActivity.java
+++ app/src/main/java/com/example/andriod/ptesjol/Activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.andriod.ptesjol.Activity;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.andriod.ptesjol.R;
 
@@ -10,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_holder_houses);
+        setContentView(R.layout.activity_main);
+
+        Toolbar tlbMain = findViewById(R.id.tlb_main);
+        setSupportActionBar(tlbMain);
+        getSupportActionBar().setTitle("Faculty of Engineering");
+
+        // Create actionbar drawer toggle
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, tlbMain, R.string.drawer_open_desc, R.string.drawer_close_desc);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
     }
 }
